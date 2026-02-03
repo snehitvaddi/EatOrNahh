dc1bd9dd8d0f58a846c0569e4a0b94151bf3063a
snehitvaddi <vaddisnehit@gmail.com>
2026-02-02 20:56:38 -0500

    Initial Commit

---
+++ EatOrNahh/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  EatOrNahh
+//
+//  Created by snehit vaddi on 02/02/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ EatOrNahhTests/EatOrNahhTests.swift
@@ -0,0 +1,16 @@
+//
+//  EatOrNahhTests.swift
+//  EatOrNahhTests
+//
+//  Created by snehit vaddi on 02/02/26.
+//
+
+import Testing
+
+struct EatOrNahhTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
